6d899824deab2abfae8cb7070197f70798c16238
Igor-G-D <igorgomesdutra@gmail.com>
2023-02-16 20:17:16 -0300

    Finished Exercise 2 - Altered the code to fit with the DOOM 3 CodeStyle convention

--- Lista_1/Ex_2.c
+++ Lista_1/Ex_2.c
@@ -4,38 +4,37 @@
 
 #define MAX 256
 
-void insere(char **baseString,char * newString);
+void Insere( char **baseString,char * newString );
 void Apaga ( char **baseString, char *toBeDeletedString );
-void imprime(char * s);
-int menu(void);
-void LerString(char palavra[MAX]);
+void Imprime( char * s ) ;
+int Menu( void );
+void LerString( char palavra[ MAX ] );
 
-int main()
-{
+int main( ) {
 	int escolha;
     char *list = NULL;
     char *name = ( char* )malloc( sizeof( char ) * 256);
     
-	for (;;) {
-		escolha = menu();
-		switch (escolha) {
+	for ( ; ; ) {
+		escolha = Menu( );
+		switch ( escolha ) {
 		case 1:
-            printf("Digite o nome a ser inserido: ");
-            LerString(name);
-			insere(&list, name);
+            printf( "Digite o nome a ser inserido: " );
+            LerString( name );
+			Insere( &list, name );
 			break;
 		case 2:
-            printf("Digite o nome a ser removido: ");
-            LerString(name);
-			Apaga(&list, name);
+            printf( "Digite o nome a ser removido: " );
+            LerString( name );
+			Apaga( &list, name );
 			break;
 		case 3:
-			imprime(list);
+			Imprime( list );
 			break;
 		case 4:
-			free(name);
-			free(list);
-			exit(0);
+			free( name );
+			free( list );
+			exit( 0 );
 			break;
 		default:
 			break;
@@ -44,87 +43,124 @@ int main()
 	return 0;
 }
 
-int menu(void)
+/*
+====================
+Menu
+
+Displays the menu, and returns the chosen number from the user
+====================
+*/
+
+int Menu( void )
 {
 	int c = 0;
 	do {
-		printf("-- MENU:\n");
+		printf("-- Menu:\n");
 		printf("\t 1. Inserir um nome\n");
 		printf("\t 2. Excluir um nome\n");
 		printf("\t 3. Listar nomes\n");
 		printf("\t 4. Sair\n");
 		printf("-- Digite sua escolha: ");
 		scanf("%d", &c);
-	} while (c <= 0 || c > 4);
-	getchar();
+	} while ( c <= 0 || c > 4 );
+	getchar( );
 	return c;
 }
 
-void LerString(char palavra[MAX])
-{
+/*
+====================
+LerString
+
+Reads a string of a maximum of 256 characters
+====================
+*/
+
+void LerString( char palavra[ MAX ] ) {
 	int i = 0;
 	char c;
 
-	c = getchar();
-	while ((c != '\n') && (i < MAX)) {
-		palavra[i++] = c;
-		c = getchar();
+	c = getchar( );
+	while ( ( c != '\n' ) && ( i < MAX ) ) {
+		palavra[ i++ ] = c;
+		c = getchar( );
 	}
-    palavra[i] = '\0';
+    palavra[ i ] = '\0';
 
 
 }
 
+/*
+====================
+Insere
+ Recieves a pointer to the address of a String that determines the base String, as well as a String that is to be added. If the base string is empty,
+ it allocates new memory and inserts the newString value into the base String, if the baseString already has content, it reallocs memory and inserts the
+ new String separated by a comma into the BaseString.
+====================
+*/
 
-
-void insere(char **baseString,char * newString)
+void Insere( char **baseString,char * newString )
 {
     int sizeBase;
-	if (*baseString == NULL) {
+	if ( *baseString == NULL ) {
         sizeBase = 0;
     } else {
         sizeBase = strlen(*baseString);
     }
-    int sizeNew = strlen(newString);
+    int sizeNew = strlen( newString );
 
-	if(sizeBase == 0) {
+	if( sizeBase == 0 ) {
         *baseString = ( char* )malloc( sizeof( char ) * sizeNew + 1);
-        strcpy(*baseString, newString);
+        strcpy( *baseString, newString );
     } else {
         int counter = 0;
 
-        (*baseString) = ( char* )realloc( (*baseString), ( (sizeBase+sizeNew + 2) * sizeof( char ) ) );
-        (*baseString)[sizeBase] = ',';
+        ( *baseString ) = ( char* )realloc( ( *baseString ), ( ( sizeBase+sizeNew + 2 ) * sizeof( char ) ) );
+        ( *baseString )[ sizeBase ] = ',';
 
-        for( int i = sizeBase+1 ; i < ( sizeBase + sizeNew + 2) ; i++ ) {
-            (*baseString)[i] = newString[counter];
+        for( int i = sizeBase+1 ; i < ( sizeBase + sizeNew + 2 ) ; i++ ) {
+            ( *baseString )[ i ] = newString[ counter ];
             counter++;
 		}	
     }
 }
 
+/*
+====================
+Apaga
+ Finds the first occurence of char *needle in char *haystack where each element is separated by ",", and alters the baseString (haystack) that was passed as a parameter
+ to exclude the needle.
+====================
+*/
+
 void Apaga ( char **baseString, char *toBeDeletedString )
 {
 	int found = 0;
-    char *tempBase = (char*)malloc((strlen(*baseString)+1) * sizeof(char));
-    strcpy(tempBase, *baseString);
-    free(*baseString);
+    char *tempBase = ( char* )malloc( ( strlen( *baseString )+1 ) * sizeof( char ) );
+    strcpy( tempBase, *baseString );
+    free( *baseString );
 	*baseString = NULL;
-    char *temp = strtok(tempBase,",");
-    while (temp != NULL) {
-        if(strcmp(temp, toBeDeletedString) != 0 || found) {
-            insere(baseString, temp);
+    char *temp = strtok( tempBase,"," );
+    while ( temp != NULL ) {
+        if( strcmp( temp, toBeDeletedString ) != 0 || found ) {
+            Insere( baseString, temp );
         } else {
 			found = 1;
 		}
-		temp = strtok(NULL,",");
+		temp = strtok( NULL,"," );
     }
-	free(temp);
-	free(tempBase);
+	free( temp );
+	free( tempBase );
 
 }
 
-void imprime(char * s)
+/*
+====================
+Imprime
+ prints out the list of stored words
+====================
+*/
+
+void Imprime( char * s )
 {
 	printf("Name List: %s\n", s);
 }
