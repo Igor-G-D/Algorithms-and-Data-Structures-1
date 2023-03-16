79eaf31a4ff4440689ede25e376028f8966d4d97
Igor-G-D <igorgomesdutra@gmail.com>
2023-03-16 03:33:15 -0300

    Fixed reset not working, also working on some other new projects

---
+++ Agenda_Somente_Ponteiros/include/library.h
@@ -0,0 +1,5 @@
+void ReadInfo ( void **pBuffer, void **dataHead );
+void ListInfo(void *pBuffer);
+int SearchInfo(void *pBuffer, char nameSearch[11]);
+int DeleteInfo(void **pBuffer, int positionToDelete);
+int Menu( void );
