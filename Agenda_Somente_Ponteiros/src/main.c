ee68ed38282236b8ddb90e8acf16c5fdd7b2cc22
Igor-G-D <igorgomesdutra@gmail.com>
2023-03-23 03:14:24 -0300

    Finished the assignment described in readme.md

--- Agenda_Somente_Ponteiros/src/main.c
+++ Agenda_Somente_Ponteiros/src/main.c
@@ -4,192 +4,37 @@
 #include "queue_library.h"
 
 int main( ) {
-	void * test = createHead();
+	void * pBuffer = malloc((sizeof(int))+(sizeof(char)*11));
+	void * data = createHead();
 
-	push(test, createNode());
-
-	// void *test1 = *front(test);
-
-	// void *nodo1 = test1;
-	// void *nodo1prev = *prev(nodo1);
-	// void *nodo1next = *next(nodo1);
-
-	push(test, createNode());
-
-	// test1 = *front(test);
-
-	// nodo1 = test1;
-	// nodo1prev = *prev(nodo1);
-	// nodo1next = *next(nodo1);
-
-	// void *nodo2 = nodo1prev;
-	// void *nodo2prev = *prev(nodo2);
-	// void *nodo2next = *next(nodo2);
-
-	push(test, createNode());
-
-	//listQueue(test);
-	// test1 = *front(test);
-
-	// nodo1 = test1;
-	// nodo1prev = *prev(nodo1);
-	// nodo1next = *next(nodo1);
-
-	// nodo2 = nodo1prev;
-	// nodo2prev = *prev(nodo2);
-	// nodo2next = *next(nodo2);
-
-	// void *nodo3 = nodo2prev;
-	// void *nodo3prev = *prev(nodo3);
-	// void *nodo3next = *next(nodo3);
-
-	push(test, createNode());
-
-	// test1 = *front(test);
-
-	// nodo1 = test1;
-	// nodo1prev = *prev(nodo1);
-	// nodo1next = *next(nodo1);
-
-	// nodo2 = nodo1prev;
-	// nodo2prev = *prev(nodo2);
-	// nodo2next = *next(nodo2);
-
-	// nodo3 = nodo2prev;
-	// nodo3prev = *prev(nodo3);
-	// nodo3next = *next(nodo3);
-
-	// void *nodo4 = nodo3prev;
-	// void *nodo4prev = *prev(nodo4);
-	// void *nodo4next = *next(nodo4);
-
-	//listQueue(test);
-
-	// test1 = *front(test);
-
-	// nodo1 = test1;
-	// nodo1prev = *prev(nodo1);
-	// nodo1next = *next(nodo1);
-
-	// nodo2 = nodo1prev;
-	// nodo2prev = *prev(nodo2);
-	// nodo2next = *next(nodo2);
-
-	// nodo3 = nodo2prev;
-	// nodo3prev = *prev(nodo3);
-	// nodo3next = *next(nodo3);
-
-	// nodo4 = nodo3prev;
-	// nodo4prev = *prev(nodo4);
-	// nodo4next = *next(nodo4);
-
-	push(test, createNode());
-
-	// test1 = *front(test);
-
-	// nodo1 = test1;
-	// nodo1prev = *prev(nodo1);
-	// nodo1next = *next(nodo1);
-
-	// nodo2 = nodo1prev;
-	// nodo2prev = *prev(nodo2);
-	// nodo2next = *next(nodo2);
-
-	// nodo3 = nodo2prev;
-	// nodo3prev = *prev(nodo3);
-	// nodo3next = *next(nodo3);
-
-	// nodo4 = nodo3prev;
-	// nodo4prev = *prev(nodo4);
-	// nodo4next = *next(nodo4);
-
-	// void *nodo5 = nodo4prev;
-	// void *nodo5prev = *prev(nodo5);
-	// void *nodo5next = *next(nodo5);
-
-	push(test, createNode());
-	
-	// test1 = *front(test);
-
-	// nodo1 = test1;
-	// nodo1prev = *prev(nodo1);
-	// nodo1next = *next(nodo1);
-
-	// nodo2 = nodo1prev;
-	// nodo2prev = *prev(nodo2);
-	// nodo2next = *next(nodo2);
-
-	// nodo3 = nodo2prev;
-	// nodo3prev = *prev(nodo3);
-	// nodo3next = *next(nodo3);
-
-	// nodo4 = nodo3prev;
-	// nodo4prev = *prev(nodo4);
-	// nodo4next = *next(nodo4);
-
-	// nodo5 = nodo4prev;
-	// nodo5prev = *prev(nodo5);
-	// nodo5next = *next(nodo5);
-
-	// void *nodo6 = nodo5prev;
-	// void *nodo6prev = *prev(nodo6);
-	// void *nodo6next = *next(nodo6);
-
-	listQueue(test);
-
-	free(pop(test));
-
-	listQueue(test);
-
-	// void * test1 = pop(test);
-	// void * test2 = pop(test);
-	// void * test3 = pop(test);
-	// void * test4 = pop(test);
-
-	// char *name1 = name(test1);
-	// char *name2 = name(test2);
-	// char *name3 = name(test3);
-
-
-	// int choice, toBeDeleted, searchResult;
-	// char name[11];
-    // void *pBuffer = malloc(sizeof(int));
-    // *((int *)pBuffer) = 0;
-    
-	// for ( ; ; ) {
-	// 	choice = Menu( );
-	// 	switch ( choice ) {
-	// 	case 1:
-	// 		ReadInfo(&pBuffer);
-	// 		break;
-	// 	case 2:
-    //         printf( "Inform the position to be removed: " );
-    //         scanf("%d",&toBeDeleted);
-	// 		if (DeleteInfo(&pBuffer,toBeDeleted) == 0) {
-	// 			printf("Invalid Position\n");
-	// 		}
-	// 		break;
-	// 	case 3:
-	// 		printf( "Inform the name of the person to be searched: " );
-    //         scanf("%s",name);
-	// 		searchResult = SearchInfo(pBuffer, name);
-
-	// 		if(searchResult == -1) {
-	// 			printf("Name not found\n");
-	// 		} else {
-	// 			printf("Name was found in position %d\n", searchResult); 
-	// 		}
-	// 		break;
-	// 	case 4:
-	// 		ListInfo(pBuffer);
-	// 		break;
-	// 	case 5:
-	// 		free(pBuffer);
-	// 		exit(0);
-	// 		break;
-	// 	default:
-	// 		break;
-	// 	}
-	// }
-	// return 0;
+	for ( ; ; ) {
+		menu(pBuffer);
+		switch ( (*choice(pBuffer)) ) {
+		case 1:
+			push(data, createNode());
+			break;
+		case 2:
+            removeNode(data, pBuffer);
+			break;
+		case 3:
+			searchInfo(data, pBuffer);
+			break;
+		case 4:
+			list(data);
+			break;
+		case 5:
+			clearList(data);
+			printf("\nData Cleared!\n\n");
+			break;
+		case 6:
+			clearList(data);
+			free(data);
+			free(pBuffer);
+			exit(0);
+			break;
+		default:
+			break;
+		}
+	}
+	return 0;
 }
--- Agenda_Somente_Ponteiros/src/queue_library.c
+++ Agenda_Somente_Ponteiros/src/queue_library.c
@@ -32,205 +32,163 @@ void *createNode(void) {
 void push(void *head, void *newNode) {
     *next(newNode) = NULL;
     *prev(newNode) = NULL;
-    if(*front(head) == NULL) { // if queue is empty, the element added is both the first and last element
-        *front(head) = newNode;
-        *back(head) = newNode;
+    if((*front(head)) == NULL) { // if queue is empty, the element added is both the first and last element
+        (*front(head)) = newNode;
+        (*back(head)) = newNode;
         (*(quantity(head)))++;
         return;
     } else if (strcmp(name(newNode),name(*back(head))) > 0) { // tests if the element will be added at the back of the queue
-        *next(newNode) = *back(head);
-        *prev(*back(head)) = newNode; 
-        *back(head) = newNode;
+        (*next(newNode)) = (*back(head));
+        (*prev(*back(head))) = newNode; 
+        (*back(head)) = newNode;
         (*(quantity(head)))++;
         return;
     } else {
-        void *p = *back(head);
+        void *p = (*back(head));
         while(*next(p) != NULL && strcmp(name(newNode), name(*next(p))) < 0 ) { // iterates until it finds where the element will be added
-            p = *next(p);
+            p = (*next(p));
         }
-        *prev(newNode) = p;
-        *next(newNode) = *next(p);
-        if(*next(p) != NULL) { // if p->next = null, it means that the element will be added in the front of the list, so it must remain NULL
-            *prev(*next(p)) = newNode;
+
+        (*prev(newNode)) = p;
+        (*next(newNode)) = (*next(p));
+        if((*next(p)) != NULL) { // if p->next = null, it means that the element will be added in the front of the list, so it must remain NULL
+            *prev((*next(newNode))) = newNode;
+            *next((*prev(newNode))) = newNode;
         } else {
-            *front(head) = newNode; // front of the queue becomes the element added in the front
+            (*next(*front(head))) = newNode;
+            (*front(head)) = newNode; // front of the queue becomes the element added in the front
         }
         (*(quantity(head)))++;
         return;
     }
-
-    // void * test1 = newNode;
-    // void * test2 = *prev(newNode);
-    // void * test3 = *next(newNode);
-
-    // void * test4 = *front(head);
-    // void * test5 = *prev(test4);
-    // void * test6 = *next(test4);
-
-    // void * test7 = *back(head);
-    // void * test8 = *prev(test7);
-    // void * test9 = *next(test7);
-    // void * test10 = NULL;
-    // if(test5) {
-    //     test10 = *prev(test5);   
-    // }
 }
 
-void *pop(void *head) { //pops the first element from the Queue, NOTE: Does not free the removed element
-    void *poppedNode;
-    void *test1 = *front(head);
-
-	void *test2 = *next(test1);
-	void *test3 = *prev(test1);
-    if(*front(head) == NULL) {
-        return NULL; // the Queue is empty
-    } else {
-        poppedNode = *front(head);
-        *front(head) = *prev(*front(head)); // with the element in the front of the queue removed, the new "front" element is the one previous to that removed element
-        if(*next(*back(head)) != NULL) { // it may already be null if it's the only element in the list
-            *next(*front(head)) = NULL; // makes the front element stop pointing to the removed element
-        } else {
-            *back(head) = NULL; // if there are no elements, both front and back are NULL
+void list(void *head) {
+    printf("\nThere are %d entries: \n \n", (*quantity(head)));
+    printf("=========================================\n");
+    void *p = *front(head);
+    while (p != NULL) {
+        printf("- - Name: %s\n", name(p));
+        printf("- - Age: %d\n", *age(p));
+        printf("- - Phone: %s\n", tel(p));
+        if(*prev(p) != NULL) { // this is only for the menu to look pretty
+            printf("=========================================\n");
         }
-        *next(poppedNode) = NULL;
-        *prev(poppedNode) = NULL;
-        (*quantity(head))--;
-        return poppedNode;
+         p = (*prev(p));
     }
 }
 
-void reset(void *head) { // clears Queue
-    for(void* p = pop(head); p != NULL; p = pop(head)) { //pops every element until it finds NULL
-        free(p);  // frees the current element
-    }
-}
-
-void listQueue(void *head) {
-    void *newHead = createHead(); // new head to insert the read elements
-    printf("There are %d elements in the Queue \n", *quantity(head));
-    while (*quantity(head) > 0) {
-        void * test4 = *front(head);
-        void * test5 = *prev(test4);
-        void * test6 = *next(test4);
-        void *poppedElement = pop(head);
-        printf("- - Name: %s\n", name(poppedElement));
-        printf("- - Age: %d\n", *age(poppedElement));
-        printf("- - Phone: %s\n", tel(poppedElement));
-        printf("=========================================\n");
-        push(newHead, poppedElement); //insert the popped elements into the new temp head
-    }
-    while(*quantity(newHead) > 0) {
-        void *poppedElement = pop(newHead);
-        push(head, poppedElement); //insert the popped elements back into head
-    }
-    *next(newHead) = NULL;
-    *prev(newHead) = NULL;
-    free(newHead);
-}
-
-
-
-// void push(void *head, void *newNode) {
+void removeNode(void *head, void * pBuffer) {
     
-//     if(*((void**)(head+sizeof(int))) == NULL) { // if queue is empty, the element added is both the first and last element
-//         *((void**)(head+sizeof(int))) = newNode; // head->front
-//         *((void**)(head+sizeof(int)+sizeof(void*))) = newNode; //head->back;
-//     } else {
-//         for(void *p = *((void**)(head + sizeof(int) + sizeof(void*))); p != NULL; p = *((void**)(p+sizeof(char)*11+sizeof(int)+sizeof(char)*14+sizeof(void*)))) {
-//             if(strcmp((char*)newNode, (char*)p) < 1){ //compares if the ascii value of the node to be added is smaller or equal to the value of the current node, it will be added before the current node
-//                 *((void**)(newNode+sizeof(char)*11+sizeof(int)+sizeof(char)*14+sizeof(void*))) = p; // newNode->next = p
-//                 *((void**)newNode+sizeof(char)*11+sizeof(int)+sizeof(char)*14) = *((void**)p+sizeof(char)*11+sizeof(int)+sizeof(char)*14); // newNode->prev = p->prev
-//                 *((void**)p+sizeof(char)*11+sizeof(int)+sizeof(char)*14) = newNode; // p->previous = newNode;
-//                 if(*((void**)p+sizeof(char)*11+sizeof(int)+sizeof(char)*14) == NULL) { //if the current node is at the end of the queue, the head needs to point to it as the back of the queue
-//                     *((void**)(head+sizeof(int)+sizeof(void*))) = newNode; //head->back = newNode;
-//                 } else {
-//                     *((void**)(*((void**)newNode+sizeof(char)*11+sizeof(int)+sizeof(char)*14)) +sizeof(char)*11+sizeof(int)+sizeof(char)*14+sizeof(void*)) = newNode; //p->previous->next = newNode;
-//                 }
-//                 *((void**)newNode+sizeof(char)*11+sizeof(int)+sizeof(char)*14) = newNode;
-//                 void *testprev = *((void**)newNode+sizeof(char)*11+sizeof(int)+sizeof(char)*14);
-//                 void *testnext = *((void**)(newNode+sizeof(char)*11+sizeof(int)+sizeof(char)*14+sizeof(void*)));
-//                 break;
-//             }
-//         }
-//         if(*((void**)(newNode+sizeof(char)*11+sizeof(int)+sizeof(char)*14+sizeof(void*))) == NULL) { // newNode-> prev == NULL if it enters this condition, means that the node wasn't added anywhere else, so it will be in the front of the list
-//             *((void**)newNode+sizeof(char)*11+sizeof(int)+sizeof(char)*14) = *((void**)(head+sizeof(int)));// newNode->previous = (*head)->front;
-//             *((void**)((*((void**)(head+sizeof(int))))+sizeof(char)*11+sizeof(int)+sizeof(char)*14+sizeof(void*))) = newNode;//head->front->next = newNode;
-//             *((void**)(head+sizeof(int))) = newNode;//(*head)->front = newNode;        
-//         }
-//     }
-//     (*((int*)head))++; //head->quantity++
-// }
+    printf( "Please inform the person's name to be removed (Max 10 characters) : " );
+    scanf( "%s", nameSearch(pBuffer)); //char toBeRemoved [11]
+    if(*front(head) == NULL) { // list is empty
+        return;
+    }
+    void *p = (*back(head));
+    while (p != NULL) {
+        if(strcmp((nameSearch(pBuffer)), (name(p))) == 0) {
+            break;
+        }
+        p = (*next(p));
+    }
+    if(p == NULL) {
+        return;
+    }
+    if((*prev(p)) != NULL) {
+        (*next(*prev(p))) = (*next(p)); // makes the element before the current point to the element after the current
+    } else {
+        (*back(head)) = (*next(p)); // if its null, means the element removed was the one in the back of the queue
+    }
+    if((*next(p)) != NULL) {
+        (*prev(*next(p))) = (*prev(p)); // makes the element after the curent point to the element before the current
+    } else {
+        (*front(head)) = (*prev(p)); // if its null, means the element removed was the one in the front of the queue
+    }
+    free(p);
+    (*(quantity(head)))--;
+}
 
-// void *pop(void *head) { //pops the first element from the Queue, NOTE: Does not free the removed element
-//     void *poppedNode;
-//     void * test2 = *((void**)(head+sizeof(int)));
-//     void * test3 = *((void**)(head+sizeof(int)+sizeof(void*)));
-//     if(*((void**)(head+sizeof(int))) == NULL) { // head->front == NULL
-//         return NULL; // the Queue is empty
-//     } else {
-//         poppedNode = *((void**)(head+sizeof(int)));//poppedNode = (*head)->front;
-//         *((void**)(head+sizeof(int))) = *((void**)poppedNode+sizeof(char)*11+sizeof(int)+sizeof(char)*14);//(*head)->front = head->front->previous; // with the element in the front of the queue removed, the new "front" element is the one previous to that removed element
-//         if(*((void**)((*((void**)(head + sizeof(int) + sizeof(void*))))+sizeof(char)*11+sizeof(int)+sizeof(char)*14+sizeof(void*)))) { // (*head)->back->next != NULL // it may already be null if it's the only element in the list
-//                 void * test3 = *((void**)(head+sizeof(int)));
-//             *((void**)((*((void**)(head+sizeof(int))))+sizeof(char)*11+sizeof(int)+sizeof(char)*14+sizeof(void*))) = NULL;//(*head)->front->next = NULL; // makes the front element stop pointing to the removed element
-//         } else {
-//             *((void**)(head + sizeof(int) + sizeof(void*))) = NULL;//(*head)->back = NULL; // if there are no elements, both front and back are NULL
-//         }
-//         *((void**)poppedNode+sizeof(char)*11+sizeof(int)+sizeof(char)*14) = NULL;//poppedNode->next = NULL;
-//         *((void**)(poppedNode+sizeof(char)*11+sizeof(int)+sizeof(char)*14+sizeof(void*))) = NULL;//poppedNode->previous = NULL;
-//         (*((int*)head))--;//(*head)->quantity--;
-//         return poppedNode;
-//     }
-// }
+void searchInfo(void *head, void * pBuffer) {
+    printf( "Please inform the person's name to be searched (Max 10 characters) : " );
+    scanf( "%s", nameSearch(pBuffer)); //char toBeRemoved [11]
+    void *p = *back(head);
 
-// void reset(void *head) { // clears Queue
-//     for(void* p= pop(head); p != NULL; p = pop(head)) { //pops every element until it finds NULL
-//         free(p);  // frees the current element
-//     }
-// }
+    while (p != NULL) {
+        if(strcmp((nameSearch(pBuffer)), (name(p))) == 0) {
+            break;
+        }
+        p = (*next(p));
+    }
+    if(p == NULL) {
+        printf("=========================================\n");
+        printf("Name not in the database\n");
+    } else {
+        printf("=========================================\n");
+        printf("- - Name: %s\n", name(p));
+        printf("- - Age: %d\n", *age(p));
+        printf("- - Phone: %s\n", tel(p));
+    }
+}
 
-// void listQueue(void *head) {
-//     void *newHead = createHead(); // new head to insert the read elements
-//     printf("There are %d entries in the database \n", (*((int*)head)));
-//     printf("=========================================\n");
-//     while ((*((int*)head)) > 0) {
-//         void *poppedElement = pop(head);
-//         printf("- - Name: %s\n", (char*)(poppedElement));
-//         printf("- - Age: %d\n", *((int*)(poppedElement+sizeof(char)*11)));
-//         printf("- - Phone: %s\n", (char*)(poppedElement+sizeof(char)*11+sizeof(int)));
-//         printf("=========================================\n");
-//         push(newHead, poppedElement); //insert the popped elements into the new temp head
-//     }
-//     while ((*((int*)newHead)) > 0) {
-//         void *poppedElement = pop(newHead);
-//         push(head, poppedElement); //insert the popped elements back into head
-//     }
-//     free(newHead);
-// }
+void clearList(void *head) {
+    void* p = *back(head);
+    while (p != NULL) {
+        void *toRemove = p;
+        p = *next(p);
+        free(toRemove);
+    }
+    *back(head) = NULL;
+    *front(head) = NULL;
+    *(quantity(head)) = 0;
+}
 
-// Functions to return specific parts of the node and head
+void menu( void *pBuffer ) {
+	do {
+        printf("=========================================\n");
+		printf( "-- Menu:\n");
+		printf( "\t 1. Insert one person's information\n" );
+		printf( "\t 2. Delete by name\n" );
+		printf( "\t 3. Search by name\n" );
+		printf( "\t 4. List all\n" );
+		printf( "\t 5. Clear data\n" );
+        printf( "\t 6. Exit\n" );
+		printf( "-- Enter your choice: " );
+		scanf( "%d", choice(pBuffer));
+        printf("=========================================\n");
+	} while ( (*choice(pBuffer)) <= 0 || (*choice(pBuffer)) > 6 );
+	getchar( );
+}
+
+// Functions to return specific parts of the node, pBuffer and head
 
 int * quantity(void * head) {
-    return ((int*)head);
+    return ((int*)(head));
 }
 void ** front(void * head) {
-    return ((void**)(head+sizeof(int))); 
+    return ((void**)((head)+(sizeof(int)))); 
 }
 void ** back(void * head) {
-    return ((void**)(head + sizeof(int) + sizeof(void*)));
+    return ((void**)((head) + (sizeof(int)) + (sizeof(void*))));
 }
 void ** next(void * node) {
-    return ((void**)(node+sizeof(char)*11+sizeof(int)+sizeof(char)*14+sizeof(void*)));
+    return ((void**)((node)+(sizeof(char)*11)+(sizeof(int))+(sizeof(char))*(14+sizeof(void*))));
 }
 void ** prev(void * node) {
-    return ((void**)node+sizeof(char)*11+sizeof(int)+sizeof(char)*14);
+    return ((void**)((node)+(sizeof(char)*11)+(sizeof(int))+(sizeof(char)*14)));
 }
 char * name(void * node) {
-    return ((char*)node);
+    return ((char*)(node));
 }
 int * age(void * node) {
-    return ((int*)(node+sizeof(char)*25));
+    return ((int*)((node)+(sizeof(char)*25)));
 }
 char * tel(void * node) {
-    return (char*)(node+sizeof(char)*11);
+    return ((char*)((node)+(sizeof(char)*11)));
+}
+int * choice(void * pBuffer) {
+    return ((int*)(pBuffer));
+}
+char * nameSearch(void * pBuffer) {
+    return ((char*)((pBuffer) + (sizeof(int))));
 }
